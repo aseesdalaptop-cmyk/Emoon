6c818b7805762771f1e23efc7a02a6a98db543f8
aseesdalaptop-cmyk <aseesdalaptop@gmail.com>
2026-01-06 12:23:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function openBook() {
+  alert("📖 Opening free ebook...\n(Imagine knowledge entering your brain)");
+}
+
+function buyBook() {
+  alert("💸 Redirecting to payment...\nWallet says: THINK AGAIN 😅");
+}
